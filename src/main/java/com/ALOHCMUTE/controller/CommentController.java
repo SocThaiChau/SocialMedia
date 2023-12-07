02cbb263d757a592bbec5844629ffd5d59239ca4
TKim <21110521@student.hcmute.edu.vn>
2023-12-07 20:27:01 +0700

    comment input image

--- src/main/java/com/ALOHCMUTE/controller/CommentController.java
+++ src/main/java/com/ALOHCMUTE/controller/CommentController.java
@@ -1,6 +1,8 @@
 package com.ALOHCMUTE.controller;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
 
 import javax.servlet.ServletException;
@@ -15,9 +17,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.ALOHCMUTE.entity.Comments;
 import com.ALOHCMUTE.service.CommentService;
+import com.ALOHCMUTE.service.FileUploadService;
 import com.ALOHCMUTE.service.UserService;
 
 @RestController
@@ -28,13 +32,16 @@ public class CommentController {
 	
 	@Autowired
 	private UserService usersService;
+	
+	@Autowired
+    private FileUploadService fileUploadService;
 
     // Display all comments
     @GetMapping("/list")
     public String listComments(Model model) {
         List<Comments> comments = commentsService.getAllComments();
         model.addAttribute("comments", comments);
-        return "comments/list";
+        return "redirect:/home";
     }
 
     // Display a specific comment
@@ -47,16 +54,22 @@ public class CommentController {
     }
 
     @PostMapping("/add-comment")
-    public String addComment(@RequestParam("content") String content,
+    public String addComment(@RequestParam("content") String content, @RequestParam("image") MultipartFile imageFile,
     							HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
         Comments comment = new Comments();
+        
+        if (!imageFile.isEmpty()) {
+            String uniqueFileName = fileUploadService.saveFile(imageFile, "D:\\Pictures");
+            comment.setImage(uniqueFileName);
+        }
+        
         comment.setContent(content);
 
         // Save the comment
         commentsService.saveComment(comment);
         
-     // tra ve views
-     		resp.sendRedirect(req.getContextPath() + "/home");
+        // tra ve views
+     	resp.sendRedirect(req.getContextPath() + "/home");
 
         return "redirect:/home";
     }
--- src/main/webapp/WEB-INF/views/comment.jsp
+++ src/main/webapp/WEB-INF/views/comment.jsp
@@ -6,17 +6,28 @@
 
 <!-- Comments Section -->
 <div class="comments mt-3 ms-4 me-5">
-    <c:forEach var="comment" items="${comments}">
+    <%-- <c:forEach var="comment" items="${comments}">
         <div>
             <p>${comment.users.username}: ${comment.content}</p>
         </div>
-    </c:forEach>
+    </c:forEach> --%>
+    
+    <!-- Display existing comments -->
+    <div class="comment">
+        <strong>John Doe:</strong> Nice picture!
+    </div>
+    <div class="comment">
+        <strong>Jane Smith:</strong> I wish I could be there.
+    </div>
 
     <!-- Form for adding new comments -->
-    <form class="comment-form mt-3" action="/comments/add-comment" method="post">
+    <form class="comment-form mt-3" action="/comments/add-comment" method="post" enctype="multipart/form-data">
         <div class="mb-3">
             <textarea class="form-control" rows="3" id="writePost" name="content" placeholder="Viết bình luận..."></textarea>
         </div>
+        <div class="mb-3">
+        <input type="file" class="form-control" id="image" name="image">
+    	</div>
         <hr class="mb-3">
         <button type="submit" class="btn btn-primary writeComment__btnComment mb-3 justify-content-between">Post Comment</button>
     </form>
