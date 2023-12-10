941b1f363e1696872b6288fad9e74b3d437de0aa
SocThai <csthai1309@gmail.com>
2023-12-10 22:18:22 +0700

    update UserRegistration

--- src/main/java/com/ALOHCMUTE/controller/UserRegistrationController.java
+++ src/main/java/com/ALOHCMUTE/controller/UserRegistrationController.java
@@ -1,5 +1,6 @@
 package com.ALOHCMUTE.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -13,6 +14,7 @@ import lombok.AllArgsConstructor;
 @Controller
 @AllArgsConstructor
 public class UserRegistrationController {
+    @Autowired
 	private IUsersService userService;
     @ModelAttribute("userdto")
     public UserDto userResgistrationDto(){
